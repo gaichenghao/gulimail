200348547f9a10b1479deb5571a07ec6ff37caee
gaich <1432351805@qq.com>
2022-01-19 17:57:26 +0800

    126

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -2,22 +2,39 @@ package com.atguigu.gulimall.search.config;
 
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 1\导入依赖
  * 2、编写配置,给容器中注入一个resthighlevelclient
- * 3、
+ * 3、参照api
  */
 
 
 @Configuration
 public class GulimallElasticSearchConfig {
 
+    public static final RequestOptions COMMON_OPTIONS;
+    
+    static {
+        RequestOptions.Builder builder=RequestOptions.DEFAULT.toBuilder();
+        //builder.addHeader("Authorization","Bearer "+TOKEN);
+        //builder.setHttpAsyncResponseConsumerFactory(
+        //        new HttpAsyncResponseConsumerFactory
+        //                .HeapBufferedResponseConsumerFactory(30*1024*1024*1024);
+                COMMON_OPTIONS=builder.build();
+
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 
     @Bean
     public RestHighLevelClient esRestClient(){
--- gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,12 +1,20 @@
 package com.atguigu.gulimall.search;
 
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 
 @RunWith(SpringRunner.class)
@@ -23,4 +31,28 @@ public class GulimallSearchApplicationTests {
         System.out.println(client);
     }
 
+    /**
+     * 测试存储数据到es
+     */
+    @Test
+    public  void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");//数据id
+        //indexRequest.source("username","zhangsna","age","18","gender","男");
+        User user=new User();
+        String s = JSON.toJSONString(user);
+        indexRequest.source(s, XContentType.JSON);//要保存的内容
+
+        //执行操作
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        //提取有用的响应数据
+        System.out.println(index);
+    }
+
+    @Data
+    class User{
+        private String username;
+        private String gender;
+        private Integer age;
+    }
 }
