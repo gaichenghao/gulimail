40867946c11b8e70ae0c7ab815d71696cd9679fc
gaich <1432351805@qq.com>
2022-02-18 18:10:11 +0800

    283

--- gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
+++ gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
@@ -7,6 +7,7 @@ public enum BizCodeEnume {
     PRODUCT_UP_EXCEPTION(11000,"商品上架异常"),
     USER_EXIST_EXCEPTION(15001,"用户存在"),
     PHONE_EXIST_EXCEPTION(15002,"手机号存在"),
+    NO_STOCK_EXCEPTION(21000,"商品库存不足"),
     lOGINACCT_PASSWORD_INVAILD_EXCEPTION(15003,"账号密码错误");
 
     private int code;
